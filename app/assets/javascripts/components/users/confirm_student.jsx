333692aa7599568adc91e8451d6673595d9e197f
Johnny Ji <johnny@johnnyji.com>
2015-05-21 17:34:38 -0700

    added styling to popup student card:

--- app/assets/javascripts/components/users/confirm_student.jsx
+++ app/assets/javascripts/components/users/confirm_student.jsx
@@ -20,7 +20,7 @@ var ConfirmStudent = React.createClass({
     var s = this.state;
     return (
       <div className={s.selected ? "resend-student-container selected" : "resend-student-container"}>
-        {p.student.first_name + " " + p.student.last_name}
+        <p className="student-name">{p.student.first_name + " " + p.student.last_name}</p>
         {s.selected && <i className="fa fa-remove" onClick={this.removeSelectedStudent}></i>}
         {!s.selected && <i className="fa fa-plus" onClick={this.selectStudent}></i>}
       </div>
