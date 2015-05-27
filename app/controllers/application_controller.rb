f47f4df07790702b00b9957d6319749c4ecb5f03
Johnny Ji <johnny@johnnyji.com>
2015-05-26 19:06:41 -0700

    .'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_filter :cors_preflight_check
-  after_action :access_control_headers
+  after_action :set_access_control_headers
 
  def set_access_control_headers
    headers['Access-Control-Allow-Origin'] = "*"
