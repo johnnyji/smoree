c035af13ee7bdfcc58dcd112a178e0cada177b8d
Johnny Ji <johnny@johnnyji.com>
2015-05-28 00:46:16 -0700

    added quotes to loading page

--- app/assets/javascripts/components/full_page_spinner.jsx
+++ app/assets/javascripts/components/full_page_spinner.jsx
@@ -1,10 +1,16 @@
 var FullPageSpinner = React.createClass({
+  propTypes: {
+    quotes: React.PropTypes.string
+  },
   render: function() {
     return (
-      <div className="sk-spinner sk-spinner-three-bounce" id="full-page-spinner">
-        <div className="sk-bounce1"></div>
-        <div className="sk-bounce2"></div>
-        <div className="sk-bounce3"></div>
+      <div>
+        <div className="sk-spinner sk-spinner-three-bounce" id="full-page-spinner">
+          <h1>{this.props.quote}</h1>
+          <div className="sk-bounce1"></div>
+          <div className="sk-bounce2"></div>
+          <div className="sk-bounce3"></div>
+        </div>
       </div>
     );
   }
--- app/assets/javascripts/components/users/load_user_page.jsx
+++ app/assets/javascripts/components/users/load_user_page.jsx
@@ -4,7 +4,14 @@ var LoadUserPage = React.createClass({
   },
   getDefaultProps: function() {
     return {
-      tabs: ["fa-list-ul", "fa-envelope", "fa-history", "fa-line-chart", "fa-cog"]
+      tabs: ["fa-list-ul", "fa-envelope", "fa-history", "fa-line-chart", "fa-cog"],
+      quotes: [
+        "All my life I thought air was free... Until I bought a bag of chips.",
+        "3 A.M. phone calls be like \"Hey are you asleep?\"... \"No, I'm skydiving.\"",
+        "I won't be impressed with technology until I can download food.",
+        "Don't think of yourself as an ugly person, think of yourself a beautiful monkey.",
+        "Sometimes I laugh so hard that no noise comes out, so I sit there clapping like a disabled seal."
+      ]
     };
   },
   getInitialState: function() {
@@ -29,6 +36,7 @@ var LoadUserPage = React.createClass({
     });
   },
   render: function() {
+    var i = Math.floor(Math.random() * this.props.quotes.length);
     if (this.state.loaded) {
       return (
         <Dashboard
@@ -39,7 +47,7 @@ var LoadUserPage = React.createClass({
         />
       );
     } else {
-      return <FullPageSpinner />
+      return <FullPageSpinner quote={this.props.quotes[i]} />
     }
   }
 });
