0123581da6d145c4e2ad7e4c36569ca6756c4eac
Johnny Ji <johnny@johnnyji.com>
2015-05-28 01:16:34 -0700

    added more loading page quotes

--- app/assets/javascripts/components/users/load_user_page.jsx
+++ app/assets/javascripts/components/users/load_user_page.jsx
@@ -6,6 +6,17 @@ var LoadUserPage = React.createClass({
     return {
       tabs: ["fa-list-ul", "fa-envelope", "fa-history", "fa-line-chart", "fa-cog"],
       quotes: [
+        "With great power comes great electricity bill.",
+        "Please don't scare me, I poop very easily.",
+        "Raisin cookies that look like chocolate chip cookies are the main reason I have trust issues.",
+        "Laughter is the best medicine. But if you're laughing for no reason, you probably need medicine.",
+        "Sometimes I feel like I need a six months vacation, twice a year.",
+        "At night I can't sleep, In the morning I can't wake up. Fantastic.",
+        "Knowledge is knowing tomato is a fruit. Wisdom is not putting tomatos in a fruit salad.",
+        "Why did kamikaze pilots wear helmets?",
+        "If you accidentally ate your tongue, what would it taste like?",
+        "Why do people say \"Heads up!\", when you're suppose to duck?",
+        "If tomatos are a fruit, does that make ketchup a smoothie?",
         "All my life I thought air was free... Until I bought a bag of chips.",
         "3 A.M. phone calls be like \"Hey are you asleep?\"... \"No, I'm skydiving.\"",
         "I won't be impressed with technology until I can download food.",
